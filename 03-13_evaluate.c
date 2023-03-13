267bff42bcf8e8f2dddd6cb9734a2e0829710451
root <root@DESKTOP-L1UKUDH.localdomain>
2023-03-13 21:21:16 +0100

    3rd commit

---
+++ 03-13_evaluate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void evaluate(int x);
+
+int main()
+{
+	int i;
+
+	printf("Type an integer value: ");
+	scanf("%d",&i);
+	evaluate(i);
+
+	return(0);
+}
+
+void evaluate(int x)
+{
+	if (x < 10){
+        printf("%d less than 10\n", x);
+	}else if (x == 10){
+        printf("%d equal to 10", x);
+	}else{
+        printf("%d greater than 10\n", x);
+	}
+}
+
